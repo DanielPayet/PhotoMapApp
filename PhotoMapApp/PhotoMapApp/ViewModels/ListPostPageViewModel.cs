65ea82b8d4e2986de4556a5827e978c54e8237b4
Daniel Payet <danieldu974@gmail.com>
2019-03-04 15:33:17 +0100

    reverse list

--- PhotoMapApp/PhotoMapApp/ViewModels/ListPostPageViewModel.cs
+++ PhotoMapApp/PhotoMapApp/ViewModels/ListPostPageViewModel.cs
@@ -5,6 +5,7 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Xamarin.Forms;
 
@@ -14,22 +15,29 @@ namespace PhotoMapApp.ViewModels
 	{
         private IPostService _postService;
         private IImageService _imageService;
-        private List<Post> _posts = new List<Post>();
+        private static ObservableCollection<Post> _posts = null;
 
-        public List<Post> Posts
+        public ObservableCollection<Post> Posts
         {
-            get { return _posts; }
+            get {
+                if (_posts == null) {
+                    _posts = new ObservableCollection<Post>();
+                }
+                return _posts;
+            }
             set { SetProperty(ref _posts, value); }
         }
         
         public DelegateCommand<Post> OpenPostCommand { get; private set; } 
+        public DelegateCommand inverseListCommand { get; private set; }
 
         public ListPostPageViewModel(INavigationService navigationService, IPostService postService, IImageService imageService): base (navigationService)
         {
             this._postService = postService;
             this._imageService = imageService;
-            this.Posts = postService.GetPosts();
+            this.Posts = new ObservableCollection<Post>(OrderedList(postService.GetPosts()));
             OpenPostCommand = new DelegateCommand<Post>(OpenPostDetail);
+            inverseListCommand = new DelegateCommand(ReverseList);
         }
 
         private void OpenPostDetail(Post post)
@@ -38,5 +46,16 @@ namespace PhotoMapApp.ViewModels
             navigationParam.Add("post", post);
             base.NavigationService.NavigateAsync("PostPage", navigationParam);
         }
-	}
+
+        private List<Post> OrderedList(List<Post> posts)
+        {
+            posts.OrderBy((x) => x.DateTime);
+            return posts;
+        }
+
+        private void ReverseList()
+        {
+            this.Posts = new ObservableCollection<Post>(Posts.Reverse());
+        }
+    }
 }
